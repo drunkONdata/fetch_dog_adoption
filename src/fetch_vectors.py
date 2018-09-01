52283a60cdf3dc0b64c9d6c3bb5167f41382face
Abhi Banerjee <bil2ab@gmail.com>
2018-08-31 17:47:41 -0700

    Crashed EC2. modified code to restart at original spot.

--- src/fetch_vectors.py
+++ src/fetch_vectors.py
@@ -16,7 +16,7 @@ import time
 import requests
 from io import BytesIO
 import io
-import glob
+#import glob
 #import sys
 #import multiprocessing
 ImageFile.LOAD_TRUNCATED_IMAGES = True
@@ -25,10 +25,10 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 def run_durka():
     image_path_list = pd.read_pickle('../data/fetch_img_urls.pkl', compression='gzip')
     #feature_matrix = np.zeros((len(image_path_list),4096))
-    start = len(glob.glob1('../data/feature_vec/','*.jpg'))
+    #start = len(glob.glob1('../data/feature_vec/','*.jpg'))
     model = initialize_neural_network()
 
-    for idx,img in enumerate(image_path_list[start:len(image_path_list)]):
+    for idx,img in enumerate(image_path_list[3279:len(image_path_list)]):
         #feature_matrix[idx] = vectorize_image(img, model)
         vectorize_image(img, model)
     #pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
@@ -75,5 +75,6 @@ def create_feature_matrix():
         feature_matrix[idx] = np.load('../data/feature_vec/feature_vec_'+img_name.split('.')[0]+'.npy')
        
     end = time.time()
-''' 
+    print('Features matrix created. Time: '+str(end-start))
     
+'''
